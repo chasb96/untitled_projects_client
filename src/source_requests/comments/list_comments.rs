6fc63d9b68a7cf90fbdb57330d6c8018c1e71754
Charles Bassett <chasb96@gmail.com>
2024-11-22 17:52:28 -0600

    Fix visibility

--- src/source_requests/comments/list_comments.rs
+++ src/source_requests/comments/list_comments.rs
@@ -3,15 +3,15 @@ use prost::Message;
 #[derive(Message)]
 pub struct ListCommentsResponse {
     #[prost(message, repeated, tag = "1")]
-    comments: Vec<CommentResponse>,
+    pub comments: Vec<CommentResponse>,
 }
 
 #[derive(Message)]
 pub struct CommentResponse {
     #[prost(string, tag = "1")]
-    id: String,
+    pub id: String,
     #[prost(string, tag = "2")]
-    user_id: String,
+    pub user_id: String,
     #[prost(string, tag = "3")]
-    content: String,
+    pub content: String,
 }
